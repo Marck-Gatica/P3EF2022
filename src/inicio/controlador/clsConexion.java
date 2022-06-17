2c78f246040cf53bf13df0b986b2e576e9b91a74
Marco Gatica <mgaticac@miumg.edu.gt>
2022-06-17 16:10:58 -0600

    Agregado de frmLogin y MDI

---
+++ src/inicio/controlador/clsConexion.java
@@ -0,0 +1,49 @@
+
+package inicio.controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Edwin
+ */
+public class clsConexion {
+        private static final String JDBC_URL = "jdbc:mysql://localhost/sic?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "123456";
+   
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+    }
+    
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+    
+    
+}
